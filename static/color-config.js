2934f1b981de3834edb650f6c9d15daeeaf7ffcf
Kyle Brown <picokyle@gmail.com>
2016-03-28 01:00:24 -0600

    add colouring for literals

--- static/color-config.js
+++ static/color-config.js
@@ -5,5 +5,7 @@ $(function() {
     $(".python-import").css("color", $("#import-color").val());
     $(".python-return").css("color", $("#return-color").val());
     $(".python-name").css("color", $("#name-color").val());
+    $(".python-number").css("color", $("#number-color").val());
+    $(".python-string").css("color", $("#string-color").val());
   });
 });
