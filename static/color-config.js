5ff41c252fc220ef9552a07db7cddc28f399ad1f
Kyle Brown <picokyle@gmail.com>
2016-03-28 00:52:21 -0600

    add return, basic expressions

--- static/color-config.js
+++ static/color-config.js
@@ -3,6 +3,7 @@ $(function() {
     $(".python-keyword").css("color", $("#keyword-color").val());
     $(".python-def").css("color", $("#def-color").val());
     $(".python-import").css("color", $("#import-color").val());
+    $(".python-return").css("color", $("#return-color").val());
     $(".python-name").css("color", $("#name-color").val());
   });
 });
