d3e268941fe85ff32866494098de1cfa76f066f1
Kyle Brown <picokyle@gmail.com>
2016-03-28 03:13:54 -0600

    fix spacing, stub in decorators

--- static/color-config.js
+++ static/color-config.js
@@ -4,6 +4,7 @@ $(function() {
     $(".python-def").css("color", $("#def-color").val());
     $(".python-import").css("color", $("#import-color").val());
     $(".python-return").css("color", $("#return-color").val());
+    $(".python-decorator").css("color", $("#decorator-color").val());
     $(".python-name").css("color", $("#name-color").val());
     $(".python-number").css("color", $("#number-color").val());
     $(".python-string").css("color", $("#string-color").val());
