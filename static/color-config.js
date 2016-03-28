6f7bf8a15eb29d62f7b75b8a9e4a755cef4a090e
Kyle Brown <picokyle@gmail.com>
2016-03-27 22:19:21 -0600

    add function definition headers

--- static/color-config.js
+++ static/color-config.js
@@ -1,6 +1,8 @@
 $(function() {
   $("#updateColors").click(function() {
     $(".python-keyword").css("color", $("#keyword-color").val());
+    $(".python-def").css("color", $("#def-color").val());
+    $(".python-import").css("color", $("#import-color").val());
     $(".python-name").css("color", $("#name-color").val());
   });
 });
