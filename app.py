15f25a08d9000cc6e6c6ee230656a9436d223063
Kyle Brown <picokyle@gmail.com>
2016-03-31 11:29:13 -0600

    cleanup
    Use blank_line instead of consecutive new_line and move operator
    name to symbol mapping into a filter.

--- app.py
+++ app.py
@@ -24,6 +24,22 @@ app = Flask(__name__)
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
+def op_name_filter(op_name):
+    return {
+        'Add': '+',
+        'Sub': '-',
+        'Mult': '*',
+        'Div': '/',
+        'FloorDiv': '//',
+        'Mod': '%',
+        'Eq': '==',
+        'Lt': '<',
+        'Gt': '>',
+        'LtE': '<=',
+        'GtE': '>='
+    }[op_name]
+app.jinja_env.filters['op_name'] = op_name_filter
+
 @app.route('/')
 def index():
     return render_template('input.html')
