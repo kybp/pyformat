b40f28dce6199b875645507d4d83665f55d794dc
Kyle Brown <picokyle@gmail.com>
2016-03-31 03:53:25 -0600

    fix up color selection
    Add a checkbox to each color saying whether that color should be set
    or not. If not, I should also disable the corresponding color picker,
    but I haven't done that yet. The color-table code has been cleaned up
    considerably with a Javascript function and a Jinja2 macro. The color
    updating respects the hierarchy, eg True is a python-true, then a
    python-constant, then a python-keyword, now that colors can be not
    set.
    
    name-constant was renamed to constant throughout.

--- static/color-config.js
+++ static/color-config.js
@@ -1,24 +1,33 @@
 $(function() {
-  $("#updateColors").click(function() {
-    $(".python-keyword").css("color", $("#keyword-color").val());
-    $(".python-def").css("color", $("#def-color").val());
-    $(".python-class").css("color", $("#class-color").val());
-    $(".python-import").css("color", $("#import-color").val());
-    $(".python-return").css("color", $("#return-color").val());
-    $(".python-decorator").css("color", $("#decorator-color").val());
-    $(".python-name").css("color", $("#name-color").val());
-    $(".python-name-constant").css("color", $("#name-constant-color").val());
-    $(".python-None").css("color", $("#None-color").val());
-    $(".python-for").css("color", $("#for-color").val());
-    $(".python-while").css("color", $("#while-color").val());
-    $(".python-if").css("color", $("#if-color").val());
-    $(".python-else").css("color", $("#else-color").val());
-    $(".python-false").css("color", $("#false-color").val());
-    $(".python-true").css("color", $("#true-color").val());
-    $(".python-and").css("color", $("#and-color").val());
-    $(".python-or").css("color", $("#or-color").val());
-    $(".python-number").css("color", $("#number-color").val());
-    $(".python-pass").css("color", $("#pass-color").val());
-    $(".python-string").css("color", $("#string-color").val());
-  });
+  var updateColor = function(name) {
+    if ($("#" + name + "-is-set").is(":checked")) {
+      $(".python-" + name).css("color", $("#" + name + "-color").val());
+    }
+  };
+
+  var updateAllColors = function() {
+    updateColor("keyword");
+    updateColor("def");
+    updateColor("class");
+    updateColor("import");
+    updateColor("return");
+    updateColor("decorator");
+    updateColor("name");
+    updateColor("constant");
+    updateColor("None");
+    updateColor("for");
+    updateColor("while");
+    updateColor("if");
+    updateColor("else");
+    updateColor("false");
+    updateColor("true");
+    updateColor("and");
+    updateColor("or");
+    updateColor("number");
+    updateColor("pass");
+    updateColor("string");
+  };
+
+  $("#updateColors").click(updateAllColors);
+  updateAllColors();
 });
